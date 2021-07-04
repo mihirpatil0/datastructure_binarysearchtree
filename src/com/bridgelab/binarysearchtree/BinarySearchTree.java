66c754a9627b3aba153d6e78f1c3fe5ed3d93cc9
mihirpatil0 <mihirrpatil@gmail.com>
2021-07-05 03:11:37 +0530

    [mihir] Added: creating binary search tree by creating 3 nodes.

--- src/com/bridgelab/binarysearchtree/BinarySearchTree.java
+++ src/com/bridgelab/binarysearchtree/BinarySearchTree.java
@@ -10,5 +10,68 @@ package com.bridgelab.binarysearchtree;
 
 public class BinarySearchTree
 {
-
+	// Root of BST
+    Node root;
+ 
+    // Constructor
+    BinarySearchTree()
+    {
+         root = null;
+    }
+ 
+    // This method mainly calls insertRec()
+    void insert(int key)
+    {
+         root = insertRec(root, key);
+    }
+ 
+    /* A recursive function to
+       insert a new key in BST */
+    Node insertRec(Node root, int key)
+    {
+ 
+        /* If the tree is empty,
+           return a new node */
+        if (root == null)
+        {
+            root = new Node(key);
+            return root;
+        }
+ 
+        /* Otherwise, recur down the tree */
+        if (key < root.key)
+            root.left = insertRec(root.left, key);
+        else if (key > root.key)
+            root.right = insertRec(root.right, key);
+ 
+        /* return the (unchanged) node pointer */
+        return root;
+    }
+ 
+    // This method mainly calls printRecursion()
+    void printTree()
+    {
+         printRecursion(root);
+    }
+ 
+    void printRecursion(Node root)
+    {
+        if (root != null) {
+            printRecursion(root.left);
+            System.out.println(root.key);
+            printRecursion(root.right);
+        }
+    }
+ 
+    public static void main(String[] args)
+    {
+        BinarySearchTree tree = new BinarySearchTree();
+ 
+        tree.insert(56);
+        tree.insert(30);
+        tree.insert(70);
+ 
+        // print traversal of the BST
+        tree.printTree();
+    }
 }
