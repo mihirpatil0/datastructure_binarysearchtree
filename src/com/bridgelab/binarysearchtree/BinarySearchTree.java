ca9c2e770a9f836aa5c46bb65dea490404aefb2a
mihirpatil0 <mihirrpatil@gmail.com>
2021-07-05 01:46:22 +0530

    [mihir] Added : empty project structure for binary search tree.

---
+++ src/com/bridgelab/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,14 @@
+package com.bridgelab.binarysearchtree;
+
+/**************************************************************
+ * @author mihir
+ *
+ * This class represent binary search tree.
+ * Defines methods of how to insert nodes in binary search tree.
+ * and how to find specific element in a tree.
+ **************************************************************/
+
+public class BinarySearchTree
+{
+
+}
---
+++ src/com/bridgelab/binarysearchtree/Node.java
@@ -0,0 +1,13 @@
+package com.bridgelab.binarysearchtree;
+
+/*********************************************
+ * @author mihir
+ * 
+ * This class is representing nodes
+ * which are present in a binary tree
+ * that is root node, left node and right node.
+ *********************************************/
+public class Node
+{
+	
+}
